f55c25b78748c8a6b8c211778e50f081e6e0133d
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-08-22 15:34:03 -0700

    Update DMXDetectorConstruction.cc

--- src/DMXDetectorConstruction.cc
+++ src/DMXDetectorConstruction.cc
@@ -197,7 +197,7 @@ G4VPhysicalVolume* DMXDetectorConstruction::Construct() {
   //part 1
   G4Box* solidSap = new G4Box("solidSap", 0.5*S_w, 0.5*S_w, 0.5*S_l);
   logicSap = new G4LogicalVolume(solidSap, sapphireNCrystal_mat, "logicSap");  //sapphireNCrystal_mat
-  physSap = new G4PVPlacement(0, G4ThreeVector(1*m,0.,(0.5*(V_l+S_l)+P_l)-0.5*(P_p-P_w)), logicSap, "physSap", logicS, false, 0);
+  physSap = new G4PVPlacement(0, G4ThreeVector(1*m,0.,(0.5*(V_l+S_l)+P_l)-0.5*(P_p-P_w)), logicSap, "physSap", logicWorld, false, 0);
   //part 2
   //G4Box* solidSap2 = new G4Box("solidSap2", 0.5*S_w, 0.5*S_w, 0.5*S_l);
   //logicSap2 = new G4LogicalVolume(solidSap2, sapphireNCrystal_mat, "logicSap2");  //sapphireNCrystal_mat
