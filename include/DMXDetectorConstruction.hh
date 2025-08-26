83ce240fc0875bd5a83166df87d6443fb9e7a54a
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-08-26 10:26:16 -0700

    add LAr test volume

--- include/DMXDetectorConstruction.hh
+++ include/DMXDetectorConstruction.hh
@@ -180,6 +180,10 @@ private:
   G4VPhysicalVolume* physSap2;
   G4LogicalVolume*   logicAr;
   G4VPhysicalVolume* physAr;
+  G4LogicalVolume*   logicAr2;
+  G4VPhysicalVolume* physAr2;
+  G4LogicalVolume*   logicAr3;
+  G4VPhysicalVolume* physAr3;
   
 
 
