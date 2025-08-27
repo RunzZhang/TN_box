9ca5c252469ebd1d5018b6c24359f8d02208a24a
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-08-27 15:12:02 -0700

    Update DMXDetectorConstruction.cc

--- src/DMXDetectorConstruction.cc
+++ src/DMXDetectorConstruction.cc
@@ -184,8 +184,7 @@ G4VPhysicalVolume* DMXDetectorConstruction::Construct() {
   // Filter
   G4Box* solidPoly = new G4Box("solidPoly", 0.5*(2*P_w+V_l), 0.5*(2*P_w+V_l), 0.5*(P_w+V_l+P_p)); //0.5*(P_w+V_l+P_l+S_l)
 //  G4Box* vacuumNotch = new G4Box("vacuumNotch", 0.5*S_w, 0.5*S_w, 0.5*(P_p-P_l-S_l)); //0.5*(P_w+V_l+P_l+S_l) //0.5*(P_p-P_l-S_l)
-  G4Box* vacuumNotch = new G4Tubs("vacuumNotch",
-                                   0,           // inner radius
+  G4Box* vacuumNotch = new G4Tubs("vacuumNotch",0,           // inner radius
                                    0.5*S_w,      // outer radius
                                    0.5*(P_p-P_l-S_l),  // half-length along Z
                                    0,           // start angle
