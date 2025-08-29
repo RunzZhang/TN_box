4284f9c6daa475e12686727c203398e005b81f82
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-08-29 16:53:35 -0700

    Update DMXDetectorConstruction.cc

--- src/DMXDetectorConstruction.cc
+++ src/DMXDetectorConstruction.cc
@@ -208,11 +208,11 @@ G4VPhysicalVolume* DMXDetectorConstruction::Construct() {
   // Sapphire Window
   G4Box* solidSap = new G4Box("solidSap", 0.5*S_w, 0.5*S_w, 0.5*S_l);
   logicSap = new G4LogicalVolume(solidSap, sapphireNCrystal_mat, "logicSap");  //sapphireNCrystal_mat
-   physSap1 = new G4PVPlacement(0, G4ThreeVector(0.,V_l+P_p,0.5*(S_l+V_l+2*P_l)+0.5*(P_p-P_w)), logicSap, "physSap1", logicS, false, 0);
+   physSap2 = new G4PVPlacement(0, G4ThreeVector(0.,V_l+P_p,0.5*(S_l+V_l+2*P_l)+0.5*(P_p-P_w)), logicSap, "physSap2", logicS, false, 0);
 
 
 
-   physSap2 = new G4PVPlacement(&rot2, G4ThreeVector(0,0.5*(S_l+V_l+2*P_l),+0.5*(P_p-P_w)), logicSap, "physSap2", logicS, false, 0);
+   physSap1 = new G4PVPlacement(&rot2, G4ThreeVector(0,0.5*(S_l+V_l+2*P_l),+0.5*(P_p-P_w)), logicSap, "physSap1", logicS, false, 0);
 
 //   G4Tubs* solidSap = new G4Tubs("solidSap",
 //                                   0,           // inner radius
