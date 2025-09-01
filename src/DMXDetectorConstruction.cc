23470c20f7fce6e9e4d55e3d7364098116ba50e8
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2025-09-01 16:04:34 -0700

    Update DMXDetectorConstruction.cc

--- src/DMXDetectorConstruction.cc
+++ src/DMXDetectorConstruction.cc
@@ -195,7 +195,8 @@ G4VPhysicalVolume* DMXDetectorConstruction::Construct() {
 //                                   2*M_PI);     // full cylinder
   G4ThreeVector trans1 = G4ThreeVector(0.,V_l+P_p,0.5*(V_l+P_p+P_w-N_l)); // make z+ side the vaccum notch touch the Poly edge
   G4ThreeVector trans2 = G4ThreeVector(0,0.5*(V_l+2*P_p-N_l),+0.5*(P_p-P_w)); // make z+ side the vaccum notch touch the Poly edge
-  G4RotationMatrix rot1 = G4RotationMatrix(0.,0.,0.);
+  G4RotationMatrix rot1;
+  rot1.rotateX(0*deg);
   G4RotationMatrix rot2;
   rot2.rotateX(90*deg);
   G4Transform3D transform1 = G4Transform3D(rot1,trans1);
